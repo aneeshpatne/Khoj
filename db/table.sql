f39b3cd7cbfe4bf27c8cfa505e10595a69c91963
Aneesh <aneeshpatne12@gmail.com>
2025-09-09 06:25:38 +0000

    feat: making demo table

--- db/table.sql
+++ db/table.sql
@@ -1,7 +1,14 @@
 CREATE TABLE products (
-id SERIAL PRIMARY KEY,
-name TEXT NOT NULL,
-description TEXT,
-price INT NOT NULL,
-in_stock BOOLEAN NOT NULL DEFAULT true
-)
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT,
+  price_cents INT NOT NULL,
+  in_stock BOOLEAN NOT NULL DEFAULT true
+);
+
+SELECT * FROM products;
+
+INSERT INTO products (name, description, price_cents, in_stock)
+VALUES ('Coffee Mug', 'Ceramic mug', 899, true);
+
+SELECT * FROM products;
